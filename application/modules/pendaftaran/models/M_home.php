3e9f090a42535f974927c32223c6998694b8e0e5
munazar <munazarbunda@gmail.com>
2017-09-12 04:22:56 +0700

    chart display

--- application/modules/pendaftaran/models/M_home.php
+++ application/modules/pendaftaran/models/M_home.php
@@ -4,7 +4,7 @@ class M_home extends CI_Model{
 
 	function get_i_statistik_kunjungan()
 	{
-		$tgl=$_GET['tgl']!=''?$_GET['tgl']:date("Y-m-d");
+		$tgl=$_GET['tgl']!=''?date("Y-m-d",strtotime($_GET['tgl'])):date("Y-m-d");
 		
 		return $this->db->query("SELECT 
 								pli.nama_poliklinik poli, IFNULL(jn.jumlah,0) jumlah
@@ -13,7 +13,7 @@ class M_home extends CI_Model{
 								SELECT rk.kode_poliklinik k_pli, count(pk.nomor_kunjungan) jumlah FROM pendaftaran_kunjungan pk
 								INNER JOIN rajal_kunjungan rk ON rk.nomor_kunjungan=pk.nomor_kunjungan
 								INNER JOIN admin_masterpoliklinik pl ON pl.id_poliklinik=rk.kode_poliklinik
-								WHERE pk.jenis_kunjungan IN('Rajal') AND pk.tgl_daftar IN(DATE_FORMAT('".$tgl."','%Y-%m-%d'))
+								WHERE pk.jenis_kunjungan IN('Rajal') AND pk.tgl_daftar IN('".$tgl."')
 								GROUP BY rk.kode_poliklinik) jn ON jn.k_pli=pli.id_poliklinik");
 	}
 }
